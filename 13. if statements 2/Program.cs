2e1f199c32b4052cda8725b2443fbd80395104ca
Lenovo-User <Lenovo-User@DESKTOP-DGL558B>
2023-10-09 19:50:41 +0200

    Add project files.

---
+++ 13. if statements 2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13.if_statements_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetMax(2, 10));
+            Console.ReadLine();
+        }
+        static int GetMax(int num1, int num2)
+        {
+            if (num1 > num2)
+            {
+                return num1;
+            }
+            else { return num2; }
+        }
+    }
+}
